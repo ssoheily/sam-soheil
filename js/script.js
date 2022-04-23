54c2c95ee8d2b9c1c8246e725579c78228e358ee
ssoheily <sam.soheil55@gmail.com>
2022-04-23 13:40:05 +0200

    create digital-clock

--- js/script.js
+++ js/script.js
@@ -39,3 +39,30 @@ window.onload=function checkThema(){
 }
 
 /* <!-- ***** Switch End ***** --> */
+
+/* <!-- ***** Clock Start ***** --> */
+let hour = document.querySelector("#hour");
+let minute = document.querySelector("#minute");
+let seconds = document.querySelector("#seconds");
+
+setInterval(function () {
+  let dateToday = new Date();
+  let h = dateToday.getHours();
+  let m = dateToday.getMinutes();
+  let s = dateToday.getSeconds();
+  if (h < 10) {
+    h = "0" + h;
+  }
+  if (m < 10) {
+    m = "0" + m;
+  }
+  if (s < 10) {
+    s = "0" + s;
+  }
+  hour.innerHTML = h;
+  minute.innerHTML = m;
+  seconds.innerHTML = s;
+}, 1000);
+
+/* <!-- ***** Clock End ***** --> */
+
