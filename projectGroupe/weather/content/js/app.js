d84f3cf6de50e1189a1f49b3b53a41003bc3042d
ssoheily <sam.soheil55@gmail.com>
2022-05-05 12:23:05 +0200

    add project weather in row2-box

---
+++ projectGroupe/weather/content/js/app.js
@@ -0,0 +1,48 @@
+
+   
+
+    let $ = document;
+
+    let inputElem = $.querySelector(".search-box");
+    let cityElem = $.querySelector(".city");
+    let dateElem = $.querySelector(".date");
+    let tempElem = $.querySelector(".temp");
+    let weatherElem = $.querySelector(".weather");
+    let hi_lowElem = $.querySelector(".hi-low");
+    inputElem.addEventListener("keypress",(e)=>{  
+        if(e.keyCode===13){
+            fetchData();           
+        }
+    })
+    let apiData ={
+        url:"https://api.openweathermap.org/data/2.5/weather?q=",
+        key:"a2b3a0415be7b655273c68ac7ef2206e"
+    }
+
+function fetchData () {
+    let countryValue= inputElem.value
+    fetch(`${apiData.url}${countryValue}&&appid=${apiData.key}`)
+    .then(res=> res.json())
+    .then(data=> {
+        showData(data);
+        console.log(data);
+    })
+}
+function showData (data) {
+    cityElem.innerHTML=`${data.name}, ${data.sys.country} `;
+    dateElem.innerHTML= showDate();
+    tempElem.innerHTML= `${Math.floor( data.main.temp - 273.15)}°c ` //kelvin to celcios
+    weatherElem.innerHTML= `${data.weather[0].main}`
+    hi_lowElem.innerHTML= `${Math.floor(data.main.temp_min- 273.15)}°c / ${Math.floor(data.main.temp_max- 273.15)}°c`;
+}
+
+function showDate () {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let now= new Date();
+    let day =  days[now.getDay()];
+    let month = months[now.getMonth()];
+    let year = now.getFullYear(); 
+    let date = now.getDate()
+    return `  ${day} ${date} ${month} ${year} `
+}
