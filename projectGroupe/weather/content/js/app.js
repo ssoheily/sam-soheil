f71025129e05eecc4562bae14d07cf9059f9744f
ssoheily <sam.soheil55@gmail.com>
2022-05-25 09:17:12 +0200

    revert to brfore Animation icon-bar , add link games to  btn-global

--- projectGroupe/weather/content/js/app.js
+++ projectGroupe/weather/content/js/app.js
@@ -8,12 +8,30 @@ let weatherElem = $.querySelector(".weather");
 let hi_lowElem = $.querySelector(".hi-low");
 let Get_Device_Location = $.querySelector(".Get_Device_Location");
 inputElem.addEventListener("keypress", (e) => {
-  if (e.keyCode === 13) {
-    fetchData();
-  }
+    if (e.keyCode === 13) {
+          checkInputValue(e)
+    }
 });
+
+function checkInputValue(){
+    if(inputElem.value){
+      fetchData();
+    }
+    else{
+      Swal.fire({
+        title: 'Error!',
+        text: 'City Name !',
+        icon: 'error',
+        confirmButtonText: 'Write City Name Please'
+      })
+    
+    }
+  
+}
+
 let apiData = {
-  url: "https://api.openweathermap.org/data/2.5/weather?q=",
+  url:"https://api.openweathermap.org/data/2.5/weather?q=",
+        key:"a2b3a0415be7b655273c68ac7ef2206e"
 };
 
 function fetchData() {
@@ -22,17 +40,28 @@ function fetchData() {
     .then((res) => res.json())
     .then((data) => {
       showData(data);
-      console.log(data);
+      inputElem.value="";
     });
 }
 function showData(data) {
-  cityElem.innerHTML = `${data.name}, ${data.sys.country} `;
-  dateElem.innerHTML = showDate();
-  tempElem.innerHTML = `${Math.floor(data.main.temp - 273.15)}°c `; //kelvin to celcios
-  weatherElem.innerHTML = `${data.weather[0].main}`;
-  hi_lowElem.innerHTML = `${Math.floor(
-    data.main.temp_min - 273.15
-  )}°c / ${Math.floor(data.main.temp_max - 273.15)}°c`;
+  if(data.message==="city not found"){
+    Swal.fire({
+      title: 'Error!',
+      text: 'The city name is not correct!',
+      icon: 'error',
+      confirmButtonText: 'Change Name Please'
+    })
+  
+  }
+  else{
+    cityElem.innerHTML = `${data.name}, ${data.sys.country} `;
+    dateElem.innerHTML = showDate();
+    tempElem.innerHTML = `${Math.floor(data.main.temp - 273.15)}°c `; //kelvin to celcios
+    weatherElem.innerHTML = `${data.weather[0].main}`;
+    hi_lowElem.innerHTML = `${Math.floor(
+      data.main.temp_min - 273.15
+    )}°c / ${Math.floor(data.main.temp_max - 273.15)}°c`;
+  }
 }
 
 function showDate() {
@@ -68,5 +97,8 @@ function showDate() {
 }
 
 Get_Device_Location.addEventListener("click",()=>{
-    showData(data);
+  checkInputValue();
 })
+
+
+
